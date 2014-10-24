8bb6a11669be3de79d9c0ea773a9e3169df7cc33
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-10-23 17:11:17 -0700

    Add SQL to get all completed polls for user

--- polls_app/app/models/user.rb
+++ polls_app/app/models/user.rb
@@ -15,10 +15,43 @@ class User < ActiveRecord::Base
   primary_key: :id
   )
   
+  def completed_polls
 
+
+    # SELECT
+    #   polls.*
+    # FROM
+    #   polls
+    # JOIN
+    #   questions ON polls.id = questions.poll_id
+    # LEFT OUTER JOIN
+    #   answer_choices ON questions.id = answer_choices.question_id
+    # JOIN
+    #   responses ON responses.answer_choice_id = answer_choices.id
+    # JOIN
+    #
+    #   (SELECT
+    #     polls.id user_poll_id,
+    #     COUNT(questions.id) AS user_poll_response_count
+    #   FROM
+    #     polls
+    #   JOIN
+    #     questions ON polls.id = questions.poll_id
+    #   GROUP BY
+    #     polls.id) AS questions_per_poll
+    #
+    # ON
+    #   questions_per_poll.user_poll_id = polls.id
+    # WHERE
+    #   responses.user_id = 6
+    # GROUP BY
+    #   polls.id
+    # HAVING
+    #   COUNT(questions.id) = user_poll_response_count
+
+
+  end
+  
 end
 
 
-
-
-  
